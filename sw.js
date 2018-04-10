cae222497795f74d79f413a6fd594ea663adc42e
Andreas Gerlach <andi@appelgriebsch.org>
2018-04-10 23:55:46 +0200

    advanced serviceworker caching application assets

--- sw.js
+++ sw.js
@@ -1,28 +1,32 @@
 //This is the "Offline page" service worker
-const CACHE_NAME = "todolist-v1";
+const CACHE_NAME = "todolist-v2";
+const LOCAL_APP_ASSETS = [
+  "index.html",
+  "styles/todos.css",
+  "styles/uikit.almost-flat.min.css",
+  "scripts/backbone-min.js",
+  "scripts/backbone-pouch.min.js",
+  "scripts/jquery-2.0.0.min.js",
+  "scripts/pouchdb-nightly.min.js",
+  "scripts/todos.js",
+  "scripts/uikit.min.js",
+  "scripts/underscore-min.js"
+];
 
-//Install stage sets up the offline page in the cahche and opens a new cache
+//Install stage sets up the cache of the local application assets
 self.addEventListener("install", function(event) {
-  var offlinePage = new Request("offline.html");
   event.waitUntil(
-    fetch(offlinePage).then(function(response) {
-      return caches.open(CACHE_NAME).then(function(cache) {
-        console.log("Cached offline page during Install" + response.url);
-        return cache.put(offlinePage, response);
-      });
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(LOCAL_APP_ASSETS);
     })
   );
 });
 
-//If any fetch fails, it will show the offline page.
-//Maybe this should be limited to HTML documents?
+// if the request matches something in the cache deliver the cached version, otherwise call the network
 self.addEventListener("fetch", function(event) {
   event.respondWith(
-    fetch(event.request).catch(function(error) {
-      console.error("Network request Failed. Serving offline page " + error);
-      return caches.open(CACHE_NAME).then(function(cache) {
-        return cache.match("offline.html");
-      });
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
